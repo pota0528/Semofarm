0df0d88997f2e9726fc0ff27fd0884fa210e3163
pota0528 <https://github.com/pota0528>
2022-03-12 15:30:31 +0900

    Add Navigation Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import AppShell from './AppShell'; //화면에 AppShell.js에서 설정한 내용을 보이게 설정
 
 //React.Component를 상속받아 기본함수인 render함수 안에 실제 화면에 출력될 내용을 나타냄
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Hellow World!</h2>
-            </div>
+            <AppShell/> //사용자가 메인페이지인 App.js에 접속하면 AppShell의 내용 출력
         );
     }
 }
---
+++ src/components/AppShell.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles'; //material-ui 직접 디자인 설정
+import AppBar from '@material-ui/core/AppBar'; //네비게이션 바 생성
+import Drawer from '@material-ui/core/Drawer'; //네비게이션을 보이게 해줌
+import MenuItem from '@material-ui/core/MenuItem'; //네비게이션에 들어가는 아이템
+import IconButton from '@material-ui/core/IconButton'; //Appbar의 왼쪽에 들어갈 아이콘
+import MenuIcon from '@material-ui/icons/Menu'; //material-ui의 메뉴를 불러옴
+
+const styles = { //위에서 withStyles를 import 했으므로 직접 만든 스타일 적용 가능
+    root: {      //CSS와 같은 역할
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: 'auto' //네비게이션 바의 왼쪽에 메뉴버튼을 넣기 위해 왼쪽정렬
+    }
+}
+
+
+class AppShell extends React.Component {  
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false
+        }; //리액트의 컴포넌트를 상속받아 state로 toggle이라는 변수를 생성
+    }     //Drawer가 네비게이션이 현재 열려있는 상태인지 아닌지 알려주는 역할
+    handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+    // 이 함수가 수행되면 토글 변수 값을 switch 해줌
+    render() {  //render 함수를 이용해 classes 안에 별도의 디자인을 넣을 수 있도록 설정
+        const { classes } = this.props; 
+        return ( //실제 네이게이션 바가 어떻게 보여지게 할지 설정
+            <div className={classes.root}> 
+                <AppBar position="static">
+                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                        <MenuIcon/> 
+                    </IconButton>
+                </AppBar>
+                <Drawer open={this.state.toggle}>
+                    <MenuItem onClick={this.handleDrawerToggle}>Home</MenuItem>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(AppShell);
